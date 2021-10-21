f61b888257064e18c5c7437a4439edb5f0d14c14
Zachary <zachness2003@gmail.com>
2021-10-21 16:35:05 -0500

    Created basic server webpage

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 
+const express = require('express');
+const app = express();
+
+app.use(express.static(__dirname + '/website'));
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/website/index.html');
+
+});
+
+app.listen(8080, '0.0.0.0');
