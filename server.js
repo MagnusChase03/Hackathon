c4354d928cee1c078ba87c7393ec4ca27d239845
Zachary <zachness2003@gmail.com>
2021-11-12 20:12:47 +0000

    Readded EJS

--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
+const ejs = require('ejs');
+
 const app = express();
 
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.send("Test");
+    res.render('pages/index');
 
 });
 
