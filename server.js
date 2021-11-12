94c0ab265ab778a9d70a592e713ece5a75b38f1f
Zachary <zachness2003@gmail.com>
2021-11-12 18:48:54 +0000

    Removed EJS

--- server.js
+++ server.js
@@ -1,14 +1,11 @@
 const express = require('express');
-const ejs = require('ejs');
-
 const app = express();
 
 app.use(express.static('public'));
-app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
 
-    res.render('pages/index');
+    res.send("Test");
 
 });
 
