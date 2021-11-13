ffcfe335eee9a39dca466bf3da56d06ae962e468
Zachary <zachness2003@gmail.com>
2021-11-13 13:47:37 -0600

    Can find best professor for certain class

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -8,15 +8,17 @@ router.get('/grades/:semester/:year', function(req, res, next) {
   var semester = req.params["semester"];
   var year = req.params["year"];
   
+  // Sending Data file
   var dataLocation = "data/" + semester[0].toUpperCase() + semester.slice(1) + " " + year + "/" + semester + ""   + year + ".json"
   fs.readFile(dataLocation, (err, data) => {
 
-    res.json (JSON.parse(data.toString()));
+    res.json(JSON.parse(data.toString()));
 
   });
 
 });
 
+// Returns grades containing proffesor name
 router.get('/grades/:semester/:year/:prof', function(req, res, next) {
 
   var semester = req.params["semester"];
@@ -28,6 +30,7 @@ router.get('/grades/:semester/:year/:prof', function(req, res, next) {
 
       var grades = JSON.parse(data.toString());
       
+      // Finding grades with matching professor
       var matches = [];
       for (var grade in grades) {
 
@@ -41,6 +44,42 @@ router.get('/grades/:semester/:year/:prof', function(req, res, next) {
       }
 
       res.json(matches);
+
+  });
+
+});
+
+// Returns professor with most A+ in class
+router.get('/grades/best/:semester/:year/:subj/:class', function(req, res, next) {
+
+  var semester = req.params["semester"];
+  var year = req.params["year"];
+  var subject = req.params["subj"];
+  var classNumber = req.params["class"];
+  
+  var dataLocation = "data/" + semester[0].toUpperCase() + semester.slice(1) + " " + year + "/" + semester + ""   + year + ".json"
+  console.log(dataLocation);
+
+  fs.readFile(dataLocation, (err, data) => {
+
+      var grades = JSON.parse(data.toString());
+      
+      // Finding grades with matching professor
+      var bestGradeCount = 0;
+      var bestGrader = grades[0];
+
+      for (var grade in grades) {
+
+        if (grades[grade]["subj"] == subject && grades[grade]["num"] == classNumber && grades[grade]["grades"]["A+"] > bestGradeCount) {
+
+            bestGradeCount = grades[grade]["grades"]["A+"];
+            bestGrader = grades[grade];
+
+        }
+
+      }
+
+      res.json(bestGrader);
 
   });
 
