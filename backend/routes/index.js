0c1cb5a64846e7a75a948bf3c559efaf15f82682
Zachary <zachness2003@gmail.com>
2021-11-13 13:33:30 -0600

    Can filter through UTDGrades

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,49 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+// Returns grades
+router.get('/grades/:semester/:year', function(req, res, next) {
+
+  var semester = req.params["semester"];
+  var year = req.params["year"];
+  
+  var dataLocation = "data/" + semester[0].toUpperCase() + semester.slice(1) + " " + year + "/" + semester + ""   + year + ".json"
+  fs.readFile(dataLocation, (err, data) => {
+
+    res.json (JSON.parse(data.toString()));
+
+  });
+
+});
+
+router.get('/grades/:semester/:year/:prof', function(req, res, next) {
+
+  var semester = req.params["semester"];
+  var year = req.params["year"];
+  var professor = req.params["prof"]
+  
+  var dataLocation = "data/" + semester[0].toUpperCase() + semester.slice(1) + " " + year + "/" + semester + ""   + year + ".json"
+  fs.readFile(dataLocation, (err, data) => {
+
+      var grades = JSON.parse(data.toString());
+      
+      var matches = [];
+      for (var grade in grades) {
+
+        var grader = grades[grade];
+        if (grader["prof"].includes(professor)) {
+
+            matches.push(grader);
+
+        }
+
+      }
+
+      res.json(matches);
+
+  });
+
 });
 
 module.exports = router;
