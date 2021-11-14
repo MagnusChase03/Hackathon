62c08b80443b23b2854444085ee7b7e6bda513c5
Zachary <zachness2003@gmail.com>
2021-11-14 07:00:37 -0600

    Style

--- frontend/pages/classInfo.js
+++ frontend/pages/classInfo.js
@@ -13,22 +13,39 @@ export default function ClassInfo() {
         <title>UTD *name pending*</title>
         <meta name="description" content="We got kicked out of our room so we made this!" />
         <link rel="icon" href="/favicon.ico" />
+        <link rel="stylesheet" href="css/bootstrap.css" />
+        <link rel="stylesheet" href="css/main.css" />
       </Head>
 
       <nav className={navStyle.navBar}>
-        <ul>
-          <li className={navStyle.navTitle}><a href='/'><img className={navStyle.logo} src="/assets/logo.png"></img></a></li>
-          <li><Link href='/classInfo'><a>Class Information</a></Link></li>
-          <li><Link href='/studentOrgs'><a>Student Organizations</a></Link></li>
-          <li><Link href='/about'><a>About</a></Link></li>
-        </ul>
+          <ul>
+              <li className={navStyle.navTitle}><a href='/'><img className={navStyle.logo} src="/assets/logo.png"></img></a></li>
+              <li><Link href='/classInfo'><a>Class Information</a></Link></li>
+              <li><Link href='/studentOrgs'><a>Student Organizations</a></Link></li>
+              <li><Link href='/about'><a>About</a></Link></li>
+          </ul>
       </nav>
 
       <main className={styles.main}>
-        <h1>Class Information</h1>
-        <p>Need grade info, professor ratings, or any other notable content on a class?Enter it in the field below!</p>
-        <div className={styles.SearchForm}>
-          <SearchForm />
+
+          <div class="container-fluid">
+
+            <div class="row">
+
+              <div class="col-sm-1"></div>
+
+              <div class="col-sm-10" id="content">
+
+                <h1>Class Information</h1><hr />
+
+                <SearchForm />
+
+              </div>
+
+              <div class="col-sm-1"></div>
+
+            </div>
+
         </div>
 
       </main >
--- frontend/pages/components/searchForm.js
+++ frontend/pages/components/searchForm.js
@@ -75,20 +75,53 @@ function testElements(stringJSON, string2) {
 
 
         let elements = (
-            <div className={style.courseCard}>
-                <ul>
-                    <li>Course: {json["subj"]} {json["num"]}</li>
-                    <li>Instructor: {json["prof"]}</li>
-                    <li>A: {a1 + a2 + a3}</li>
-                    <li>B: {b1 + b2 + b3}</li>
-                    <li>C: {c1 + c2 + c3}</li>
-                    <li>D: {d1 + d2 + d3}</li>
-                    <li>F: {Number(json["grades"]['F']) || 0}</li>
-                    <li>Rating: {json2["rating"]}</li>
-                    <li># of Ratings: {json2["numberOfRatings"]}</li>
-                    <li>Difficulty: {json2["difficulty"]}</li>
-                    <li>% Would Take Again: {json2["wouldTakeAgain"]}</li>
-                </ul>
+            // <div className={style.courseCard}>
+            //     <ul>
+            //         <li>Course: {json["subj"]} {json["num"]}</li>
+            //         <li>Instructor: {json["prof"]}</li>
+            //         <li>A: {a1 + a2 + a3}</li>
+            //         <li>B: {b1 + b2 + b3}</li>
+            //         <li>C: {c1 + c2 + c3}</li>
+            //         <li>D: {d1 + d2 + d3}</li>
+            //         <li>F: {Number(json["grades"]['F']) || 0}</li>
+            //         <li>Rating: {json2["rating"]}</li>
+            //         <li># of Ratings: {json2["numberOfRatings"]}</li>
+            //         <li>Difficulty: {json2["difficulty"]}</li>
+            //         <li>% Would Take Again: {json2["wouldTakeAgain"]}</li>
+            //     </ul>
+            // </div>
+
+            <div class="container-fluid">
+
+                <div class="row organizationRow">
+
+                    <div class="col-sm-8">
+
+                        <h3>Course: {json["subj"]} {json["num"]}</h3>
+                        <b>Instructor: {json["prof"]}</b>
+
+                    </div>
+                    <div class="col-sm-2">
+
+                        <b>A: {a1 + a2 + a3}</b><br/>
+                        <b>B: {b1 + b2 + b3}</b><br/>
+                        <b>C: {c1 + c2 + c3}</b><br/>
+                        <b>D: {d1 + d2 + d3}</b><br/>
+                        <b>F: {Number(json["grades"]['F']) || 0}</b><br/>
+
+                    </div>
+
+                    <div class="col-sm-2">
+
+                        <b>Rating: {json2["rating"]}</b><br/>
+                        <b># of Ratings: {json2["numberOfRatings"]}</b><br/>
+                        <b>Difficulty: {json2["difficulty"]}</b><br/>
+                        <b>% Would Take Again: {json2["wouldTakeAgain"]}</b><br/>
+
+                    </div>
+
+                </div>
+
             </div>
 
 
@@ -222,7 +255,7 @@ export default class SearchForm extends Component {
             <div className="search-form">
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        Semester
+                        <span class="inputField">Semester: </span>
                         <select name='semester' value={this.state.value} onChange={this.handleChange} required>
                             <option value="fall">Fall</option>
                             <option value="spring">Spring</option>
@@ -230,7 +263,7 @@ export default class SearchForm extends Component {
                     </label>
 
                     <label>
-                        Year
+                        <span class="inputField">Year: </span>
                         <select name='year' value={this.state.value} onChange={this.handleChange} required>
                             <option value="2017">2017</option>
                             <option value="2018">2018</option>
@@ -238,27 +271,27 @@ export default class SearchForm extends Component {
                             <option value="2020">2020</option>
                             <option value="2021">2021</option>
                         </select>
-                    </label>
+                    </label><br/><br/>
 
                     <label>
-                        Subject
+                    <span class="inputField">Subject: </span>
                         <input type="text" name='subject' value={this.state.subject} onChange={this.handleChange} />
                     </label>
 
                     <label>
-                        Course
+                    <span class="inputField">Course: </span>
                         <input type="text" name='course' value={this.state.course} onChange={this.handleChange} />
-                    </label>
+                    </label><br/><br/>
 
                     <label>
-                        Professor
+                    <span class="inputField">Professor: </span>
                         <input type="text" name='professor' value={this.state.professor} onChange={this.handleChange} />
                     </label>
 
                     <label>
-                        Best
+                        <span class="inputField">Best: </span>
                         <input type="checkbox" name="best" value={this.state.best} onChange={this.handleChange} />
-                    </label>
+                    </label><br/><br/>
 
                     <input type="submit" value="Submit" />
                 </form>
